a9d062846c7bdc1e366da5a20f1ffd4de738739c
oakes <zsoakes@gmail.com>
2014-09-02 01:21:57 -0400

    Initial import

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject play-clj.net "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/UNLICENSE"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.zeromq/jeromq "0.3.4"]]
+  :aot [play-clj.net]
+  :main play-clj.net)
---
+++ src/play_clj/net.clj
@@ -0,0 +1,123 @@
+(ns play-clj.net
+  (:require [clojure.edn :as edn])
+  (:import [org.zeromq ZContext ZMQ])
+  (:gen-class))
+
+(def ^:private server-send-address "tcp://localhost:4707")
+(def ^:private server-receive-address "tcp://localhost:4708")
+(def ^:private client-send-address server-receive-address)
+(def ^:private client-receive-address server-send-address)
+
+(defn ^:private throw-key-not-found
+  [k]
+  (throw (Exception. (str "The keyword " k " is not found."))))
+
+(defn ^:private get-obj
+  [obj k]
+  (if (map? obj)
+    (or (get obj k)
+        (throw-key-not-found k))
+    obj))
+
+(defn ^:private get-bytes
+  [k]
+  (.getBytes (name k) ZMQ/CHARSET))
+
+(defn ^:parse recv-read
+  [socket]
+  (try (some-> (.recvStr socket) edn/read-string)
+    (catch Exception _)))
+
+(def context (delay (ZContext.)))
+(def futures (atom {}))
+
+(defn receiver?
+  [socket]
+  (= ZMQ/SUB (.getType socket)))
+
+(defn sender?
+  [socket]
+  (= ZMQ/PUSH (.getType socket)))
+
+(defn sub!
+  [socket topic]
+  (doseq [s (if (map? socket)
+              (filter receiver? (get-obj socket :sockets))
+              [socket])]
+    (.subscribe s (get-bytes topic))))
+
+(defn unsub!
+  [socket topic]
+  (doseq [s (if (map? socket)
+              (filter receiver? (get-obj socket :sockets))
+              [socket])]
+    (.unsubscribe s (get-bytes topic))))
+
+(defn broadcast!
+  [socket topic message]
+  (doseq [s (if (map? socket)
+              (filter sender? (get-obj socket :sockets))
+              [socket])]
+    (.send s (pr-str [topic message]))))
+
+(defn disconnect!
+  [socket]
+  (doseq [s (if (map? socket)
+              (get-obj socket :sockets)
+              [socket])]
+    (.destroySocket @context s)
+    (when-let [f (get @futures s)]
+      (future-cancel f)
+      (swap! futures dissoc s))))
+
+(defn server
+  ([]
+    (server server-send-address server-receive-address))
+  ([server-send-address server-receive-address]
+    (let [pub (.createSocket @context ZMQ/PUB)
+          pull (.createSocket @context ZMQ/PULL)]
+      (.bind pub server-send-address)
+      (.bind pull server-receive-address)
+      (loop []
+        (let [[topic message] (recv-read pull)]
+          (when (and topic message)
+            (.sendMore pub (name topic))
+            (.send pub message)))
+        (recur)))))
+
+(defn sender
+  ([]
+    (sender client-send-address))
+  ([address]
+    (doto (.createSocket @context ZMQ/PUSH)
+      (.connect address))))
+
+(defn receiver
+  ([callback topics]
+    (receiver callback client-receive-address topics))
+  ([callback topics address]
+    (let [socket (doto (.createSocket @context ZMQ/SUB)
+                   (.connect address))]
+      (doseq [t topics]
+        (sub! socket t))
+      (->> (loop []
+             (let [topic (.recvStr socket)
+                   message (recv-read socket)]
+               (when (and topic message)
+                 (if (map? callback)
+                   (let [execute-fn! (get-obj callback :execute-fn-on-gl!)
+                         options (get-obj callback :options)]
+                     (execute-fn! (:on-receive options)
+                                  :topic (keyword topic)
+                                  :message message))
+                   (callback (keyword topic) message))
+                 (recur))))
+           future
+           (swap! futures assoc socket))
+      socket)))
+
+(defn -main
+  [& args]
+  (future (server))
+  (sub! (receiver println [] server-send-address) :test1)
+  (broadcast! (sender server-receive-address) :test1 "Test!"))
